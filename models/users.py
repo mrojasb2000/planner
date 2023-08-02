b96ae5d29a80ae9b8dc7b81732061b95dec94f00
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-08-02 11:03:39 -0400

    chore: add user models

--- models/users.py
+++ models/users.py
@@ -8,12 +8,25 @@ class User(BaseModel):
     password: str
     events: Optional[List[Event]]
 
-
-class Config:
-    schema_extra = {
-        "example": {
-            "email": "fastapi@packt.com",
-            "username": "strong!!!",
-            "events": [],
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
         }
-    }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!",
+                "events": [],
+            }
+        }
