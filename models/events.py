e876b262dd2c3b96ecc6e9efddbf2fba1a945202
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-08-08 13:35:09 -0400

    feat: add MongoDb crud operations

--- models/events.py
+++ models/events.py
@@ -1,9 +1,10 @@
+from beanie import Document
+from typing import Optional, List
+
 from pydantic import BaseModel
-from typing import List
 
 
-class Event(BaseModel):
-    id: int
+class Event(Document):
     title: str
     image: str
     description: str
@@ -15,8 +16,32 @@ class Event(BaseModel):
             "example": {
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
-                "tags": ["python", "fastapi", "book", "lauch"],
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come "
+                               "with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
+        }
+
+    class Settings:
+        name = "events"
+
+
+class EventUpdate(BaseModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[str]
+    location: Optional[str]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come "
+                               "with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet",
             }
         }
--- models/users.py
+++ models/users.py
@@ -1,12 +1,16 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+from beanie import Document, Link
+from pydantic import BaseModel, EmailStr
 from models.events import Event
 
 
-class User(BaseModel):
+class User(Document):
     email: EmailStr
     password: str
-    events: Optional[List[Event]] = None
+    events: Optional[List[Link[Event]]] = None
+
+    class Settings:
+        name = "users"
 
     class Config:
         schema_extra = {
